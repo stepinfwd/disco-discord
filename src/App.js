7c245767c83ea2a4dc173d39c2a972d7291caaec
stepinfwd <stepinfwd@gmail.com>
2021-03-02 09:57:21 +0530

    ellaam sheriyaayi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { app } from "./features/appSlice";
+// import { app } from "./features/appSlice";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="App">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          {" "}
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <p>you have to Login</p>
+      )}
     </div>
   );
 }
